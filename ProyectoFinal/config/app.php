0d33a52d9cd47aba8890eebaf396d7345de6558d
orlandobel <orlando.bel.jimmy@gmail.com>
2019-03-25 20:08:44 -0600

    login

--- ProyectoFinal/config/app.php
+++ ProyectoFinal/config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -163,6 +163,11 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
+         * Servicios añadidos
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
+        /*
          * Package Service Providers...
          */
 
@@ -174,6 +179,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -225,6 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
---
+++ ProyectoFinal/resources/views/login/login.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.layout')
+
+@section('title')
+    <title>Login</title>
+@endsection
+
+@section('css')
+@endsection
+
+<!body>
+@section('popUp')
+    <div class="page-center">
+        <div class="page-center-in">
+            <div class="container-fluid">
+                <!form class="sign-box">
+                {!!Form::open('url'=>'/login', 'class'=>'sign-box', 'method'=>'post')!!}
+                    <div class="sign-avatar">
+                        <img src="{{asset('/img/avatar-sign.png')}}" alt="">
+                    </div>
+                    <header class="sign-title">Sign In</header>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Usuario"/>
+                    </div>
+                    <div class="form-group">
+                        <!input type="password" class="form-control" placeholder="Contraseña"/>
+                        {!!Form::text('usuario', null, ['class'=>'form-control', 'placeholder'=>'Usuario', 'id'=>'usuario'])!!}
+                    </div>
+                    <button type="submit" class="btn btn-rounded">Sign in</button>
+                    <p class="sign-note"><a href="sign-up.html">Sign up</a></p>
+                    <!--<button type="button" class="close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>-->
+                {!!Form::close!!}
+                <!/form>
+            </div>
+        </div>
+    </div><!--.page-center-->
+@endsection
+<script src="js/lib/jquery/jquery.min.js"></script>
+<script src="js/lib/tether/tether.min.js"></script>
+<script src="js/lib/bootstrap/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/app.js"></script>
+<!/body>
+</html>
