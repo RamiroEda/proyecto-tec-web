6c3637e47b251394aceb09e0d7379692d140b838
ramiroeda <ramiroestradag@gmail.com>
2019-04-16 11:07:53 -0500

    Se agrego un loader

--- ProyectoFinal2/public/Template/js/spa.js
+++ ProyectoFinal2/public/Template/js/spa.js
@@ -1,6 +1,16 @@
 function cambiarPagina(url, method, data){
     if(method == undefined) method = "get";
 
+    // elemento de loading
+
+    $("#spa-content").html(
+        '<div style="width: 100%">' +
+        '   <div style="margin: 0 50% 0 50%" class="spinner-grow text-success" role="status">' +
+        '       <span class="sr-only">Loading...</span>' +
+        '   </div>' +
+        '</div>'
+    );
+
     $.ajax({
         method: method,
         url: url,
