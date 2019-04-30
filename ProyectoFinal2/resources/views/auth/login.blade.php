f9600b4cf2ea77480ed11421a0709db2f3bc7300
orlandobel <orlando.bel.jimmy@gmail.com>
2019-04-29 21:01:20 -0500

    mensajes de error en lgoin

--- ProyectoFinal2/resources/views/auth/login.blade.php
+++ ProyectoFinal2/resources/views/auth/login.blade.php
@@ -15,6 +15,18 @@
                     <div class="sign-avatar">
                         <img src="{{asset('Template/img/avatar-sign.png')}}" alt="">
                     </div>
+                    @if(count($errors)>0)
+                        <div class="alert alert-danger alert-fill alert-close alert-dismissible fade in" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <header class="sign-title">Iniciar sesión</header>
                     <div class="form-group">
                         {!!Form::text('usuario', null ,['class'=>'form-control', 'placeholder'=>'Usuario', 'id'=>'usuario'])!!}
